61ca54e20b478657c29c0d19eca6b55fcbdd6920
Kohei Nakaji <kohei.nakaji@gocro.jp>
2014-09-23 22:38:53 +0900

    implement huffman encoder

--- huffman.py
+++ huffman.py
@@ -17,16 +17,41 @@ class Huffman:
             second = queue.pop(0)
             parent = self.__create_parent(first, second)
             self.top = parent
-
-    def value(self):
-        print self.top.value
     
     def __create_parent(self, first, second):
         parent = Node(None, first.value + second.value)
-        parent.set_children(first, second)
+        parent.set_children([first, second])
         first.set_parent(parent)
         second.set_parent(parent)
         return parent
+
+    def toCodeMap(self):
+        if self.top == None:
+            return None
+        current = self.top
+        result = {}
+        codes = [0]
+        self.__search(result, codes, current)
+        return result
+                
+    def __search(self, result, codes, node):
+        if node.item != None:
+            result[Huffman.toKey(codes)] = node.item
+            return
+
+        index = 0
+        for c in node.children:
+            codesCopy = list(codes) 
+            codesCopy.append(index)
+            self.__search(result, codesCopy, c)
+            index+=1
+    
+    @staticmethod
+    def toKey(codes):
+        result = ""
+        for c in codes:
+            result += str(c)
+        return result
 
 class Node:
     def __init__(self, item, value):
@@ -36,7 +61,5 @@ class Node:
     def set_parent(self, parent):
         self.parent = parent
 
-    def set_children(self, first, second):
-        self.first = first
-        self.second = second
-
+    def set_children(self, children):
+        self.children = children
